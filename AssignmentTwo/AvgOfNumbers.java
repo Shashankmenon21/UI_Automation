1c01234ca8d401ba9f5752924d853cc03d9a7365
Shashankmenon21 <shashankmenon21@gmail.com>
2023-01-23 20:50:56 +0530

    assingments 1&2

--- AssignmentTwo/AvgOfNumbers.java
+++ AssignmentTwo/AvgOfNumbers.java
@@ -1,5 +1,24 @@
 package AssignmentTwo;
 
-public class AvgOfNumbers {
+import java.util.Scanner;
+
+public class AvgOfNumbers{
+
+    public static void main(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the first number : ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the first number : ");
+        int num3 = scanner.nextInt();
+
+        int avg = (num1+num2+num3)/3;
+        System.out.println("Average of the three numbers is : "+avg);
+
+    }
 
 }
